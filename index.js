0a487d07d801cb5014f143ca530213a281292300
afab <cyrillsphoto@gmail.com>
2021-03-11 01:44:02 +0400

    LIVE PLEASE

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const socketIO = require('socket.io');
 const PORT = process.env.PORT || 3000;
 const INDEX = 'pub/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+const app = express();
+const server = app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
