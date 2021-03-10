6d0f45d38d6c8a47e867896affeacb9a688f7fc6
afab <cyrillsphoto@gmail.com>
2021-03-11 01:30:13 +0400

    Fuck me

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const http = require("https").createServer(app);
+const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
